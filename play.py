f8f00ff53cea5579c99fe5bdb382d82a34728cdc
jkash25 <jaitkash@gmail.com>
2023-04-16 12:31:45 -0400

    fix import

--- play.py
+++ play.py
@@ -7,27 +7,5 @@ import pandas as pd
 import os
 import runuser
 import imutils
-import dlib
-import cv2
-import imageio
-from PIL import Image
-from imutils import face_utils
-import time
-from keras.utils import np_utils, generic_utils
-import shutil
-from skimage.transform import resize
-from sklearn.utils import shuffle
-from skimage.io import imread, imsave, imshow
-import tensorflow
-import keras
-from keras import layers
-from keras.layers.convolutional import Conv3D, MaxPooling3D, Conv2D, MaxPooling2D
-from keras.layers.core import Dense, Dropout, Flatten
-from keras.models import Sequential, load_model
-from keras.layers import Activation, ZeroPadding3D, TimeDistributed, LSTM, GRU, Reshape
-from keras.utils import plot_model
-import matplotlib.pyplot as plt
-from keras.layers import BatchNormalization
-from keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
 import matplotlib.pyplot as plt
 
